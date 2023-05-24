2cbefdd669e2ab41d9e2b695d03dba340b5891f6
developer <developer01@joulestowatts.com>
2023-05-24 12:59:04 +0530

    DB chnages

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,52 +1,52 @@
-// const config = require('config.json');
-// const mysql = require('mysql2/promise');
-// const { Sequelize } = require('sequelize');
+const config = require('config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
 
-// module.exports = db = {};
+module.exports = db = {};
 
-// initialize();
+initialize();
 
-// async function initialize() {
-//     // create db if it doesn't already exist
-//     // const { host, port, user, password, database } = config.database;
+async function initialize() {
+    // create db if it doesn't already exist
+    // const { host, port, user, password, database } = config.database;
 
-//     // console.log(port,host,user,password,database)
+    // console.log(port,host,user,password,database)
 
-//     const config = {
-//         host: 'LAPTOP-DRN6DAFN\SQLEXPRESS',
-//         user: 'sa',
-//         password: 'sa123',
-//         database: 'devices'
-//     };
-//     const connection = await mysql.createConnection(config);
-//     // const connection = await mysql.createConnection({ host, port, user, password });
-
-
-//     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${config.database}\`;`);
-
-//     // connect to db
-//     // const sequelize = new Sequelize(config.database, config.user, config.password, { dialect: 'mysql' });
-//     const sequelize = new Sequelize('freedb_machine_measure', 'freedb_machine_measure', 'k4scAXn!5gxVUCA', {
-//         host: 'sql.freedb.tech',
-//         dialect: 'mysql'
-//       });
-
-//     // init models and add them to the exported db object
-//     db.User = require('../users/user.model')(sequelize);
+    const config = {
+        host: 'sql.freedb.tech',
+        user: 'freedb_machine_measure',
+        password: 'k4scAXn!5gxVUCA',
+        database: 'freedb_machine_measure'
+    };
+    const connection = await mysql.createConnection(config);
+    // const connection = await mysql.createConnection({ host, port, user, password });
 
 
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${config.database}\`;`);
+
+    // connect to db
+    // const sequelize = new Sequelize(config.database, config.user, config.password, { dialect: 'mysql' });
+    const sequelize = new Sequelize('freedb_machine_measure', 'freedb_machine_measure', 'k4scAXn!5gxVUCA', {
+        host: 'sql.freedb.tech',
+        dialect: 'mysql'
+      });
+
+    // init models and add them to the exported db object
+    db.User = require('../users/user.model')(sequelize);
 
 
 
 
-//     // init models and add them to the exported db object
-//     db.Device = require('../devices/device.model')(sequelize);
-
-//     // sync all models with database
-//     await sequelize.sync();
 
 
-// }
+    // init models and add them to the exported db object
+    db.Device = require('../devices/device.model')(sequelize);
+
+    // sync all models with database
+    await sequelize.sync();
+
+
+}
 
 
 // const { Sequelize } = require('sequelize');
@@ -73,29 +73,3 @@
 //   }
 // })();
 
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('devices', 'sa', 'sa123', {
-  host: 'LAPTOP-DRN6DAFN\SQLEXPRESS', // Replace with the server name of the remote SQL Server
-  dialect: 'mssql',
-  dialectOptions: {
-    options: {
-      encrypt: true, // Use this if you're connecting to a server with a self-signed certificate
-    },
-  },
-});
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to SQL Server');
-
-    // Perform your database operations here
-
-    await sequelize.close();
-    console.log('Connection closed');
-  } catch (error) {
-    console.error('Error:', error);
-  }
-})();
